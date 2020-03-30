2a509a99110bac2207717c9fa79916260999b65a
Tushar Matta <htusharmatta96@gmail.com>
2020-03-31 02:26:49 +0530

    updated app urls.py to include homepage

--- sample_api/userapi/urls.py
+++ sample_api/userapi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from userapi import views
 
 urlpatterns = [
+    path('', views.index),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('final/', views.user_view),
